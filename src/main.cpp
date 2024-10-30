691c430feabed63a061d50f42870f659b5854527
OminousSquash <zcabvsr@ucl.ac.uk>
2024-10-30 12:30:40 +0000

    changed indentation from 2 -> 4 on save formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,28 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main() {
     auto window = sf::RenderWindow({1920u, 1080u}, "CMake SFML Project");
     window.setFramerateLimit(144);
+    window.setPosition(sf::Vector2i(10, 20));
+    window.setSize(sf::Vector2u(300, 500));
     while (window.isOpen()) {
-        for (auto event = sf::Event(); window.pollEvent(event);) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyPressed:
+                    std::cout << "Key Code:" << event.key.code << std::endl;
+                    std::cout << "Key Scancode:" << event.key.scancode << std::endl;
+                    if (event.key.code == sf::Keyboard::D) {
+                        std::cout << "PRESSED D" << std::endl;
+                    }
+                    break;
+                default:
+                    break;
             }
         }
-        window.clear();
-        window.display();
     }
 }
